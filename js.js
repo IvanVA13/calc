2c4642b7d13f269d5225cc1490cc27f76a94a69f
Ivan Volobuev <IvanVA13@users.noreply.github.com>
2021-02-08 18:02:42 +0200

    добавил -. добавил описание действий

--- js.js
+++ js.js
@@ -12,6 +12,7 @@ const num9 = document.getElementById('num9');
 const num0 = document.getElementById('num0');
 const del = document.getElementById('del');
 const sum = document.getElementById('+');
+const sub = document.getElementById('-');
     let result = []
 const action = function () {
     const input = document.getElementById('calc-input')
@@ -42,7 +43,16 @@ const action = function () {
         inputResult.setAttribute("value", sum)
     }
     if (this.innerHTML === "-") {
-        sub()
+        let sub = 0
+        buffer =
+            inputBuffer.getAttribute("value") === ""
+            ? input.getAttribute("value")
+            : `${inputBuffer.getAttribute("value")}-${input.getAttribute("value")}`
+        inputBuffer.setAttribute("value", buffer);
+        result.push(Number(input.getAttribute("value")));
+        sub = result.reduce((acc, next)=> acc - next)
+        input.setAttribute("value", "")
+        inputResult.setAttribute("value", sub)
     }
     if (this.innerHTML === "/") {
         div()
@@ -68,3 +78,10 @@ num9.addEventListener('click', action);
 num0.addEventListener('click', action);
 del.addEventListener('click', action);
 sum.addEventListener('click', action);
+sub.addEventListener('click', action);
+
+
+/* записать значение в инпут
+при нажатии на действие сохранить в буфере значение инпута и обнулить сам инпут. 
+Также должен запомнить действие действие. При последующем нажатии должен выполнить действие,
+сохраненное в памяти и записать новое действие. */
